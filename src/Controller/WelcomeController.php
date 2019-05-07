05b17955c4fbe6919e2590e42eb2714cd0115bdd
Samuel Williams <samuel.williams8@gmail.com>
2019-05-07 17:09:50 +1000

    .

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class WelcomeController extends AbstractController
+{
+    /**
+     * @Route("/welcome", name="welcome")
+     */
+    public function index()
+    {
+        return $this->render('welcome/index.html.twig', [
+            'controller_name' => __CLASS__,
+        ]);
+    }
+}
